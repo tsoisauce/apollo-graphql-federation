e2dd247b9a66743ccf326224e971e97266d3d120
Kelvin Tsoi <ktsoi@wearfigs.com>
2019-12-07 22:32:02 -0500

    setup rake

--- services/reviews/app.rb
+++ services/reviews/app.rb
@@ -2,6 +2,16 @@
 
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'dotenv/load'
+
+# initalize activerecord DB connection
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  host: (ENV['AWS_RDS_HOST']).to_s,
+  username: (ENV['AWS_RDS_USERNAME']).to_s,
+  password: (ENV['AWS_RDS_PASSWORD']).to_s,
+  database: (ENV['AWS_RDS_DB']).to_s
+)
 
 # Graph layer for review service
 class ReviewApp < Sinatra::Base
