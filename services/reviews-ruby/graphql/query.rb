43a15ce7d458bc2480d9808be0656761ee702f77
Kelvin Tsoi <ktsoi@wearfigs.com>
2019-12-11 13:48:47 -0500

    switch to static data in resolvers

--- services/reviews-ruby/graphql/query.rb
+++ services/reviews-ruby/graphql/query.rb
@@ -3,6 +3,15 @@
 require 'graphql'
 require_relative 'types/reviews'
 
+# static sample data not using active record
+def data
+  [
+    { id: 'review-1', product_sku: 'widget:1', rating: 4, title: 'test review', review: 'A++ will do again', customer: 'johndoe@email.com' },
+    { id: 'review-2', product_sku: 'widget:2', rating: 5, title: 'could be better', review: 'didnt like it much', customer: 'janedoe@email.com' },
+    { id: 'review-3', product_sku: 'widget:3', rating: 3, title: 'test', review: 'what is this', customer: 'johndoe@email.com' }
+  ]
+end
+
 # Review queries
 class QueryType < GraphQL::Schema::Object
   description 'The query root of this review schema'
@@ -12,6 +21,7 @@ class QueryType < GraphQL::Schema::Object
   end
 
   def reviews
-    Reviews.all
+    # Reviews.all
+    data
   end
 end
