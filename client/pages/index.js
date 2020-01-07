f6f6a0d0dc50e5430cbbc0a906ad66bf1bb4d8a7
Kelvin Tsoi <ktsoi@wearfigs.com>
2020-01-06 17:54:02 -0800

    add apollo library

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,10 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
+import withData from "../lib/apollo";
+import Main from "../lib/layout";
 
-export default HomePage
+export default withData(props => {
+  return (
+    <Main>
+      test
+    </Main>
+  );
+});
